914f8c7be35b4195d30f4850ac0e199919b9b9fb
Martin_G <mgabut.pro@gmail.com>
2025-12-11 10:24:25 +0100

    projet spring

---
+++ vilanelle-boot/src/test/java/vilanelle_webapp/vilanelle_boot/VilanelleBootApplicationTests.java
@@ -0,0 +1,13 @@
+package vilanelle_webapp.vilanelle_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilanelleBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
